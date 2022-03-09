13c0876647e9053fbbaa835e64e306610c658441
Kirill Panchencko <videokirills@gmail.com>
2022-03-09 19:53:52 +0200

    git init

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, url_for
+from flask_wtf import FlaskForm
+
+app = Flask(__name__)
+
+
+@app.route("/login")
+def index():
+    return render_template("login.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
