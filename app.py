e14c1c23cbc40f6ebae41651c287cdc8cb71c04d
Kirill Panchencko <videokirills@gmail.com>
2022-03-10 22:01:02 +0200

    refactoring code and adding user to db at the registation page

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template, url_for, flash
-from forms import LoginForm, RegistrationForm
+from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import generate_password_hash, check_password_hash, Bcrypt
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import Form
+from wtforms.fields import EmailField, PasswordField, SubmitField, BooleanField, StringField
+from wtforms.validators import InputRequired, Email, Length, EqualTo, ValidationError
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret!"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///forum.db"
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 
 class User(db.Model):
@@ -31,11 +36,50 @@ class Post(db.Model):
         return f"title {self.title} id {self.id} date {self.date_post}"
 
 
+class LoginForm(FlaskForm):
+    email = StringField("Почта",
+                        validators=[InputRequired("Необходимо ввести почту"), Email("Нужно ввести почту")],
+                        render_kw={"style": "width: 100%;",
+                                   "autocomplete": "off"})
+    password = PasswordField("Пароль",
+                             validators=[InputRequired("Необходимо ввести пароль")],
+                             render_kw={"style": "width: 100%;",
+                                        "autocomplete": "off"})
+    remember = BooleanField("Запомнить меня", default=False)
+    submit = SubmitField("Войти", render_kw={"style": "width: 30%;"})
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField("Почта",
+                        validators=[InputRequired("Введите почту"), Email("Нужно ввести почту")],
+                        render_kw={"style": "width: 100%;",
+                                   "autocomplete": "off"})
+    password = PasswordField("Пароль",
+                             validators=[InputRequired("Введите пароль"),
+                                         Length(min=1, max=30)],
+                             render_kw={"style": "width: 100%;",
+                                        "autocomplete": "off"})
+    confirm_password = PasswordField("Повторите пароль",
+                                     validators=[InputRequired("Повторите пароль"),
+                                                 EqualTo("password", message="Пароли должны совпадать")])
+    username = StringField("Ваше имя", validators=[InputRequired("Введите ваше имя"),
+                                                   Length(min=1, max=30)],
+                           render_kw={"style": "width: 100%;",
+                                      "autocomplete": "off"})
+    submit = SubmitField("Зарегистрироваться")
+
+    def validation_email(self, email):
+        """Проверяет уникальность почты"""
+        email_presence = User.query.filter_by(email=email.data).first()
+        if email_presence:  # Запрос должен быть пустым, в ином случае почта уже зарегистрирована
+            raise ValidationError("Такая почта уже зарегистрирована")
+
+
 @app.route("/login", methods=["POST", "GET"])
 def login():
     login_form = LoginForm()
     if login_form.validate_on_submit():
-        return f'<h1>Email: {login_form.email.data} Password: {login_form.password.data}</h1>'
+        pass
     return render_template("login.html", form=login_form)
 
 
@@ -43,7 +87,14 @@ def login():
 def register():
     registration_form = RegistrationForm()
     if registration_form.validate_on_submit():
-        return f'<h1>Email: {registration_form.email.data} Password: {registration_form.password.data} Username {registration_form.username.data}</h1>'
+        hash_password = generate_password_hash(registration_form.password.data).decode("utf-8")
+        user = User(email=registration_form.email.data,
+                    password=hash_password,
+                    username=registration_form.username.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Аккаунт создан", category="success")
+        return redirect(url_for('login'))
     return render_template("registration.html", form=registration_form)
 
 
