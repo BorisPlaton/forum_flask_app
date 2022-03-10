06d263ab21f7b42a02c541f04f8a42ad20e06fd6
Kirill Panchencko <videokirills@gmail.com>
2022-03-10 02:34:57 +0200

    registation page and form

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 from flask import Flask, render_template, url_for
-from forms import LoginForm
+from forms import LoginForm, RegistrationForm
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret!"
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///forum.db"
+#
+# db = SQLAlchemy(app)
+#
+#
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.Text, unique=True, nullable=False)
+#     password = db.Column(db.String(30), nullable=False)
+#     username = db.Column(db.String(30), nullable=False)
+#
+#     def __repr__(self):
+#         return f"user {self.username}"
 
 
 @app.route("/login", methods=["POST", "GET"])
@@ -11,6 +25,14 @@ def login():
     if login_form.validate_on_submit():
         return f'<h1>Email: {login_form.email.data} Password: {login_form.password.data}</h1>'
     return render_template("login.html", form=login_form)
+
+
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    registration_form = RegistrationForm()
+    if registration_form.validate_on_submit():
+        return f'<h1>Email: {registration_form.email.data} Password: {registration_form.password.data} Username {registration_form.username.data}</h1>'
+    return render_template("registration.html", form=registration_form)
 
 
 if __name__ == "__main__":
--- forms.py
+++ forms.py
@@ -1,16 +1,34 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import EmailField, PasswordField, SubmitField, BooleanField, IntegerField
-from wtforms.validators import InputRequired, Email
+from wtforms import Form
+from wtforms.fields import EmailField, PasswordField, SubmitField, BooleanField, StringField
+from wtforms.validators import InputRequired, Email, Length
 
 
 class LoginForm(FlaskForm):
     email = EmailField("Почта",
-                       validators=[InputRequired("Email is required")],
+                       validators=[InputRequired("Необходимо ввести почту")],
                        render_kw={"style": "width: 100%;",
                                   "autocomplete": "off"})
     password = PasswordField("Пароль",
-                             validators=[InputRequired("Password is required")],
+                             validators=[InputRequired("Необходимо ввести пароль")],
                              render_kw={"style": "width: 100%;",
                                         "autocomplete": "off"})
     remember = BooleanField("Запомнить меня", default=False)
     submit = SubmitField("Войти", render_kw={"style": "width: 30%;"})
+
+
+class RegistrationForm(FlaskForm):
+    email = EmailField("Почта",
+                       validators=[InputRequired("Необходимо ввести почту")],
+                       render_kw={"style": "width: 100%;",
+                                  "autocomplete": "off"})
+    password = PasswordField("Пароль",
+                             validators=[InputRequired("Необходимо ввести пароль"),
+                                         Length(min=1, max=30)],
+                             render_kw={"style": "width: 100%;",
+                                        "autocomplete": "off"})
+    username = StringField("Ваше имя", validators=[InputRequired("Введите ваше имя"),
+                                                   Length(min=1, max=30)],
+                           render_kw={"style": "width: 100%;",
+                                      "autocomplete": "off"})
+    submit = SubmitField("Зарегистрироваться")
