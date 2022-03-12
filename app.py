23f01ee95e9cab816dace0d0bb1f4eed8172a722
Kirill Panchencko <videokirills@gmail.com>
2022-03-12 03:10:41 +0200

    wrote post page

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import generate_password_hash, check_password_hash, Bcrypt
 from flask_wtf import FlaskForm
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
-from wtforms.fields import EmailField, PasswordField, SubmitField, BooleanField, StringField
+from wtforms.fields import EmailField, PasswordField, SubmitField, BooleanField, StringField, TextAreaField
 from wtforms.validators import InputRequired, Email, Length, EqualTo, ValidationError
 from datetime import datetime
 
@@ -80,6 +80,17 @@ class RegistrationForm(FlaskForm):
             raise ValidationError("Такая почта уже зарегистрирована")
 
 
+class WriteArticle(FlaskForm):
+    title = StringField("Название", validators=[InputRequired("Название должно быть обязательно"),
+                                                Length(min=1, max=80)],
+                        render_kw={"style": "width: 100%;",
+                                   "autocomplete": "off"})
+    text = TextAreaField("Текст", validators=[InputRequired("Напишите что-то")],
+                         render_kw={"style": "width: 100%;",
+                                    "autocomplete": "off"})
+    submit = SubmitField("Сохранить")
+
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
@@ -93,12 +104,30 @@ def home():
 @app.route("/profile")
 @login_required
 def profile():
+    """
+    Личный кабинет пользователя.
+    Включает в себя следующую информацию:
+    Имя, почта, дата регистрации, написанные комментарии и статьи.
+    """
     return render_template("profile.html", loged=True, user=current_user)
+
+
+@app.route("/wrote_post", methods=["POST", "GET"])
+@login_required
+def wrote_post():
+    """
+    Написание статьи.
+    """
+    article_form = WriteArticle()
+    return render_template("wrote_post.html", loged=True, user=current_user, form=article_form)
 
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
-    """Авторизация пользователя"""
+    """
+    Авторизация пользователя.
+    Доступна только не авторизированным пользователем. В ином случае возвращает на главную страницу.
+    """
     if current_user.is_authenticated:  # Если пользователь уже в профиле, не разрешит зайти в меню авторизации
         return redirect(url_for('home'))
 
@@ -108,7 +137,7 @@ def login():
         user = User.query.filter_by(email=login_form.email.data).first()
         if user and check_password_hash(user.password, login_form.password.data):
             login_user(user, login_form.remember.data)  # В случае успеха пользователь входит в аккаунт
-            next_page = request.args.get("next")    # Узнаем какая страница была до авторизации, чтоб вернуться на неё
+            next_page = request.args.get("next")  # Узнаем какая страница была до авторизации, чтоб вернуться на неё
             return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash("Неверная почта или пароль", category="danger")
@@ -118,7 +147,10 @@ def login():
 
 @app.route("/register", methods=["POST", "GET"])
 def register():
-    """Регистрация пользователя"""
+    """
+    Регистрация пользователя.
+    Проверяет на уникальность почты, в случае удачи возвращает на страницу входа в аккаунт.
+    """
     if current_user.is_authenticated:  # Если пользователь уже в профиле, не разрешит зайти в меню регистрации
         return redirect(url_for('home'))
     registration_form = RegistrationForm()  # Форма регистрации
@@ -131,6 +163,7 @@ def register():
                         username=registration_form.username.data)  # Создаем пользователя и хэшируем пароль
             db.session.add(user)
             db.session.commit()
+            flash("Аккаунт создан", category="success")
         else:
             flash("Такая почта уже зарегистрирована", category="danger")
             return redirect(url_for("register"))
@@ -141,7 +174,9 @@ def register():
 @app.route("/logout")
 @login_required
 def logout():
-    """Выход из аккаунта"""
+    """
+    Выход из аккаунта.
+    """
     logout_user()
     return redirect(url_for('login'))
 
